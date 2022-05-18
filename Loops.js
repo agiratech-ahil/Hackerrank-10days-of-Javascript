dff50edcf4a48b708007e4c085d84e6b6edeff9d
agiratech-ahil <ahil.singh@agiratech.com>
2022-05-18 19:32:28 +0530

    updated loops

--- Loops.js
+++ Loops.js
@@ -1,15 +1,15 @@
 function vowelsAndConsonants(s) {
-  let c, d;
-  for (let i = 0; i <= s.length - 1; i++) {
-    c = s[i];
-    if (c == "a" || c == "e" || c == "i" || c == "o" || c == "u") {
-      console.log(c);
+  const vow = "aeiou";
+
+  var sec = "";
+
+  for (var i = 0; i < s.length; i++) {
+    if (vow.includes(s[i])) {
+      console.log(s[i]);
+    } else {
+      sec += s[i] + "\n";
     }
   }
-  for (let j = 0; j <= s.length - 1; j++) {
-    d = s[j];
-    if (!(d == "a" || d == "e" || d == "i" || d == "o" || d == "u")) {
-      console.log(d);
-    }
-  }
+
+  console.log(sec);
 }
